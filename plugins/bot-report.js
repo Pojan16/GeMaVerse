42992861e67a611d3b6fb82b2fb2402ff32552ce
Zeroz04n <89253879+Pojan16@users.noreply.github.com>
2022-07-05 00:12:23 +0700

    Add files via upload

--- plugins/bot-report.js
+++ plugins/bot-report.js
@@ -1,12 +1,18 @@
-// By RC047 :V
-
-let handler = (m, { conn, text }) => {
-    if (!text) throw 'Please enter a report'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `If you find an error message, report it using this command\n\nexample:\n${usedPrefix + command} Good afternoon owner, I found an error like the following <copy/tag the error message>`
+    if (text.length < 10) throw 'The report is too short, at least 10 characters!'
     if (text.length > 500) throw 'Sorry Text Too Long, Maximum 500 Texts!'
-    const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
-    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6285856430321@s.whatsapp.net'))
+    let teks = `*${command.toUpperCase()}!*\n\nDari : *@${m.sender.split`@`[0]}*\n\nPesan : ${text}\n`
+    conn.reply(global.owner[0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
+        contextInfo: {
+            mentionedJid: [m.sender]
+        }
+    })
+    m.reply(`️_✔️Issue has been reported to Bot Owner, if ${command.toLowerCase()} is fake or just messing around no response!_`)
+    /*const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
+    for (let jid of global.owner.filter(v => v != conn.user.jid && v != '6285856430321@s.whatsapp.net'))
     m.reply(laporan, jid)
-    m.reply('✔️Problem has been reported to the Bot Owner, fake reports will not be responded to!')
+    m.reply('✔️Masalah telah dilaporkan ke Pemilik Bot, laporan palsu tidak akan ditanggapi!')*/
 }
 handler.help = ['report'].map(v => v + ' <reason>')
 handler.tags = ['bot']
